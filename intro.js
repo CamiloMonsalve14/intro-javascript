e1f62c62d40b8fec487d6a2cb12e990a2540a6be
CamiloMonsalve14 <monsalvemaderac@gmail.com>
2023-08-09 21:59:31 -0500

    primer commit javascript

---
+++ intro.js
@@ -0,0 +1,15 @@
+let variable = 3
+let stringe = "camilo"
+let boolean = 3 + 5
+
+//           0     1       2     3    4
+let array = [2,"camilo", false, "p", 3.5]
+
+console.log(variable)
+console.log(stringe)
+console.log(boolean)
+console.log(array [1], array[4])
+console.log(array.length)
+console.log(array)
+
+
